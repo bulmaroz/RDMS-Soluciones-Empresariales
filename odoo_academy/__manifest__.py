4f9b6d3a698c1b0f934e2fefd95c5fb07d92f53f
bulmaroz <odoo@ampd1.odoo.com>
2022-09-22 22:03:45 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    
+    'license':'LGPL-3',
+    'name': 'Bulmaro academy',
+    'summary': """Bulmaro Academy app to manage Training """,
+    'description': """
+    Academy Module to manage Training:
+    -Course
+    -Sessions
+    -Attendees
+    """,
+    'autor': 'RDMS',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '1.0',
+    'depends': ['base'],
+    'data':[],
+    'demo':[],
+}
